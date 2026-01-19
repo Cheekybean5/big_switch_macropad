03f7a3bd6def532d99b38a9f521e706a0c8f2c45
Cheekybean5 <Zacging@gmail.com>
2026-01-19 15:23:14 -0700

    Add initial files

---
+++ Firmware QMK/Keymaps/default/keymap.c
@@ -0,0 +1,23 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_VOLU,   KC_MPRV,   KC_MPLY,   KC_MNXT,
+        KC_VOLD,   KC_MUTE,   KC_CALC,   KC_PSCR,   KC_SLEP,
+        KC_F2,   KC_F4,   KC_F5,   KC_F11,   KC_DEL,   KC_CAPS
+    )
+};
